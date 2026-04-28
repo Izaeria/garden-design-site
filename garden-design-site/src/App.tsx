c7828caaec365bc054c4fae0922e12a48f4f712b
Izabell <izabell.hallgren@gmail.com>
2026-04-28 13:09:17 +0200

    feat: add basic page structure with routing
    create pages for Home, About, Services, Projects and Contact
    set up routing for each page
    add placeholder content (h1) for initial structure

--- garden-design-site/src/App.tsx
+++ garden-design-site/src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/om" element={<About />} />
+      <Route path="/projekt" element={<Projects />} />
+      <Route path="/kontakt" element={<Contact />} />
+      <Route path="/tjanster" element={<Services />} />
     </Routes>
   );
 }
---
+++ garden-design-site/src/pages/Projects.tsx
@@ -0,0 +1,4 @@
+function Projects() {
+  return <h1>Projekt</h1>;
+}
+export default Projects;
