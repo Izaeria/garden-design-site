dfa2b73ab475070d60c8880aeb74f2cab5828d36
Izabell <izabell.hallgren@gmail.com>
2026-04-29 23:03:06 +0200

    feat: clearer folder structure

--- garden-design-site/src/pages/ContactPage.tsx
+++ garden-design-site/src/pages/ContactPage.tsx
@@ -1,12 +1,35 @@
 function ContactPage() {
   return (
-    <div>
-      <h1>Kontakt</h1>
-      <p>
-        Redo att förverkliga din trädgård? Kontakta mig för en första
-        konsultation.
-      </p>
-    </div>
+    <>
+      <section>
+        <h1>Kontakt</h1>
+        <p>
+          Redo att förverkliga din trädgård? Skicka ett meddelande så hör jag av
+          mig!
+        </p>
+      </section>
+
+      <section>
+        <h2>Kontaktuppgifter</h2>
+        <p>Email: ...</p>
+        <p>Telefon: ...</p>
+      </section>
+
+      <section>
+        <form>
+          <label>Namn</label>
+          <input type="text" />
+
+          <label>Email</label>
+          <input type="email" />
+
+          <label>Meddelande</label>
+          <textarea />
+
+          <button>Skicka</button>
+        </form>
+      </section>
+    </>
   );
 }
 
