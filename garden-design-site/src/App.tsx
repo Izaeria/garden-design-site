da7db7b8bff746376392a768b46b837dd776f087
Izabell <izabell.hallgren@gmail.com>
2026-04-29 23:12:28 +0200

    fix: update to correct import
    update the imports to the correct ones

--- garden-design-site/src/App.tsx
+++ garden-design-site/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ProjectsPage from "./pages/ProjectsPage";
+import ProjectsPage from "./pages/projects/ProjectsPage";
 import ContactPage from "./pages/ContactPage";
 import ServicesPage from "./pages/ServicesPage";
 
