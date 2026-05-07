ead9cf60c672f3d1c8f5d4bf61e4466adf7298a8
Izabell <izabell.hallgren@gmail.com>
2026-05-07 12:53:55 +0200

    Update to About layout for responsiveness
    Add leaf image on larger screens to make up for too much empty space

--- garden-design-site/src/components/home/About/About.tsx
+++ garden-design-site/src/components/home/About/About.tsx
@@ -1,22 +1,21 @@
 import "./About.css";
 import { Link } from "react-router-dom";
 import profileImg from "../../../assets/profile.jpeg";
+import leaf from "../../../assets/leaf.png";
 
 function About() {
   return (
     <section id="about" className="about">
-      <div className="about-image-wrapper">
-        <img src={profileImg} className="profile-image" alt="" />
-      </div>
       <div className="about-content">
+        <img src={profileImg} className="profile-image" alt="profilbild" />
+
         <p className="about-label">Om mig</p>
         <h2 className="about-title">Hej, jag heter Jennifer</h2>
-        <p className="about-text">
+        <p className="about-text-top">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
           faucibus eros. Cras et facilisis felis, a tristique nibh.
         </p>
-        <p className="about-text">
-          {" "}
+        <p className="about-text-bottom">
           Nam eu mattis lorem. Suspendisse eget posuere urna. Interdum et
           malesuada fames ac ante ipsum primis in faucibus.
         </p>
@@ -24,6 +23,7 @@ function About() {
           Läs mer om mig
         </Link>
       </div>
+      <img src={leaf} className="about-leaf-image" alt="" />
     </section>
   );
 }
